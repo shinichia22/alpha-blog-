5dfd5bbb86d2c870127c60c6f7c6249eeaefdfb6
rs_sonirajesh <shinichiappu@gmail.com>
2019-12-29 22:58:34 +0530

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,6 @@ class  ArticlesController < ApplicationController
     @any_name =Article.all
   end
 
-
-
   def new
     @article =Article.new
   end
@@ -19,18 +17,18 @@ class  ArticlesController < ApplicationController
     # @article =Article.new(article_params)  #This code is without validation
     # @article.save                             #This code is without validation
     # redirect_to article_path(@article)       #This code is without validation
-    @article =Article.new(article_params)
+    @article =Article.new(article_params)  # this is taken from the new action
     if @article.save 
       flash[:notice] = "Article has successfully created"
-      redirect_to article_path(@article)
+      redirect_to article_path(@article)  #Show path
     else
       render 'new'
     end
   end
   
-  def update
-    @article = Article.find(params[:id])
-    if @article.update(article_params)
+  def update  
+    @article = Article.find(params[:id])  # This is taken care from the edit action
+    if @article.update(article_params)  # as in routes it shows that it take arguments we add the arguments
       flash[:notice] = "Article has been successfully updated"
       redirect_to article_path(@article)
     else
@@ -42,6 +40,15 @@ class  ArticlesController < ApplicationController
   def show
     @article=Article.find(params[:id])
   end
+
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article has successfully deleted"
+    redirect_to articles_path
+
+  end
+
 
 private
   def article_params
