0074c44694d82d4629183e1d218bc72e087becc2
rs_sonirajesh <shinichiappu@gmail.com>
2019-12-29 14:11:15 +0530

    Craeted articles, tables, validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 3, maximum: 30}
+  validates :description, presence:true, length: {minimum:5, maximum: 300}
 
 end
