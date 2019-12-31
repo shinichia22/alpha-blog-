852d5c336c819ab51f5a9d2e2359f133ba8b83d9
rs_sonirajesh <shinichiappu@gmail.com>
2020-01-01 00:34:45 +0530

    Styling the erros and message partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class  ArticlesController < ApplicationController
     # redirect_to article_path(@article)       #This code is without validation
     @article =Article.new(article_params)  # this is taken from the new action
     if @article.save 
-      flash[:notice] = "Article has successfully created"
+      flash[:success] = "Article has successfully created"
       redirect_to article_path(@article)  #Show path
     else
       render 'new'
@@ -30,7 +30,7 @@ class  ArticlesController < ApplicationController
   def update  
     # @article = Article.find(params[:id])  # This is taken care from the edit action
     if @article.update(article_params)  # as in routes it shows that it take arguments we add the arguments
-      flash[:notice] = "Article has been successfully updated"
+      flash[:success] = "Article has been successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -44,7 +44,7 @@ class  ArticlesController < ApplicationController
 
   def destroy    
     @article.destroy
-    flash[:notice] = "Article has successfully deleted"
+    flash[:danger] = "Article has successfully deleted"
     redirect_to articles_path
 
   end
